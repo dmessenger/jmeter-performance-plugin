d33d1676917eb32b2e223df6acd7cf72043d233f
agile <agile@8Z9D45J.eal.wiley.com>
2012-09-06 15:51:47 +0100

    US000|dmessenger|Initial shell Mojo

---
+++ src/main/java/org/dmessenger/jmeter/performance/plugin/JMeterPerformanceMojo.java
@@ -0,0 +1,18 @@
+package org.dmessenger.jmeter.performance.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Delete and Add Ldap entries from an ldif file
+ * 
+ * @goal verify
+ */
+public class JMeterPerformanceMojo extends AbstractMojo {
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		
+	}
+
+}
